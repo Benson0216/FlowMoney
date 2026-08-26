9a9137bbe8ed7a566b52a68994f93ff2660ca93d
Benson Lee <BensonLee@Besnons-MacBook-Air.local>
2026-08-26 21:17:39 +0800

    test: add transaction model tests

---
+++ FlowMoneyTests/FlowMoneyTests.swift
@@ -0,0 +1,18 @@
+//
+//  FlowMoneyTests.swift
+//  FlowMoneyTests
+//
+//  Created by Benson Lee on 2026/8/26.
+//
+
+import Testing
+
+struct FlowMoneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
