e34bb6ac68afba938957a86c1050b16829540bf3
Benson0216 <benson0042@gmail.com>
2026-09-20 10:17:07 +0800

    test: add transaction editing coverage

--- FlowMoneyTests/TransactionDetailViewTests.swift
+++ FlowMoneyTests/TransactionDetailViewTests.swift
@@ -37,4 +37,43 @@ final class TransactionDetailViewTests: XCTestCase {
 
         XCTAssertNotNil(view)
     }
+
+    func testTransactionDetailViewUpdatesTransaction() throws {
+        let transaction = Transaction(
+            amount: 150,
+            currencyCode: "TWD",
+            type: .expense,
+            date: Date.now,
+            merchantName: "Coffee Shop",
+            categoryName: "Food",
+            category: nil,
+            paymentMethod: .creditCard,
+            note: nil,
+            source: .manual
+        )
+
+        let mockService = MockTransactionService()
+        let viewModel = TransactionViewModel(
+            service: mockService
+        )
+
+        viewModel.transactions = [transaction]
+
+        try viewModel.updateTransaction(
+            transaction,
+            amount: 200,
+            currencyCode: "TWD",
+            type: .expense,
+            date: transaction.date,
+            merchantName: "Starbucks",
+            categoryName: "Food",
+            category: nil,
+            paymentMethod: .creditCard,
+            note: nil,
+            source: .manual
+        )
+
+        XCTAssertEqual(transaction.amount, 200)
+        XCTAssertEqual(transaction.merchantName, "Starbucks")
+    }
 }
